55cde305378d8e983d69763888b4c0729140bb12
Traiphakh stihhikaew <sirprak1245@gmail.com>
2023-12-30 19:55:02 +0700

    fix:  changed from pnpm to npm for resolve error package manager

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,9 @@ import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.min.css'
 
 // Form Validator
+import '@formkit/themes/genesis'
 import { plugin, defaultConfig } from '@formkit/vue'
+
 
 const vuetify = createVuetify({
   components,
